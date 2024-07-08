74fba910d3d818b2ac1fede5f7674ac56d67ebab
EduardOrellana <ore23006@byui.edu>
2024-07-08 11:21:57 -0600

    updating data

---
+++ chamber/scripts/join.js
@@ -0,0 +1,10 @@
+import Footer_and_HamBurger  from "./base.js";
+
+const principalDynamic = new Footer_and_HamBurger();
+
+principalDynamic.setfooter();
+principalDynamic.refreshPage();
+principalDynamic.setHambutton();
+
+/////////////////////////////////////////////////////////////////////////////
+
