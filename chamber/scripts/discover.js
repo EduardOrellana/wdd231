24ef3c6a73b3ee6ebc23d05c4e2e8073b03749c8
EduardOrellana <ore23006@byui.edu>
2024-07-12 18:33:41 -0600

    siteplan of autoErorella page

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,11 @@
+import Footer_and_HamBurger  from "./base.js";
+
+const principalDynamic = new Footer_and_HamBurger();
+
+principalDynamic.setfooter();
+principalDynamic.refreshPage();
+principalDynamic.setHambutton();
+
+/////////////////////////////////////////////////////////////////////////////
+
+//More details with memberships levels
