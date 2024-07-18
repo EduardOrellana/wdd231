31f4e12d27d90c8258ba2fae3d115b7edd003cc0
EduardOrellana <ore23006@byui.edu>
2024-07-17 22:07:55 -0600

    desinging

---
+++ project/scripts/base.js
@@ -0,0 +1,37 @@
+//This file is the principal script to set the footer and the hambutton and refreshPage.
+
+class Footer_and_HamBurger {
+
+    setfooter(){
+
+        const currentYear = document.getElementById('currentyear');
+        const lastModified = document.getElementById('modified');
+    
+        currentYear.textContent = new Date().getFullYear();
+        lastModified.textContent = `Last modification: ${document.lastModified}`;
+
+    }
+
+    setHambutton(){
+
+        const hamButton = document.querySelector("#menu");
+        const navigation = document.querySelector("#navMenu");
+        
+        hamButton.addEventListener('click', () => {
+            navigation.classList.toggle('open');
+            hamButton.classList.toggle('open');
+            this.classList.toggle('open');
+        });
+
+    }
+
+    refreshPage(){
+        const logoMain = document.querySelector("#logo-page");
+
+        logoMain.addEventListener('click', () =>{
+            location.reload();
+        })
+    }
+}
+
+export default Footer_and_HamBurger;
---
+++ project/scripts/home.js
@@ -0,0 +1,7 @@
+import Footer_and_HamBurger from "./base.js";
+
+const principalDynamic = new Footer_and_HamBurger();
+
+principalDynamic.setfooter();
+principalDynamic.refreshPage();
+principalDynamic.setHambutton();
